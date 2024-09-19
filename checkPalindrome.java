143ed1f5b4f0d1d3d292739bdd9b71a6203fbdb5
musaraf0101 <nowsathmusaraf037@gmail.com>
2024-09-19 22:14:35 +0530

    oop assidnment

---
+++ checkPalindrome.java
@@ -0,0 +1,10 @@
+public class checkPalindrome {
+    public static void main(String[] args) {
+        String name = "musaraf";
+        boolean isname = true;
+        String fullname = isname.toLowercase();
+
+        for(int i=0;i<)
+        
+    }
+}
