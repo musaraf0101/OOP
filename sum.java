5bb06401b17a2e1b768693129e383d10dbaa2591
musaraf0101 <nowsathmusaraf037@gmail.com>
2024-09-19 21:12:43 +0530

    oop assingment

---
+++ sum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Min 3 digit number");
+        int number = sc.nextInt();
+        String min = Integer.toString(number);
+        for(int i =0;i<min.length();i++){
+            System.out.println(min.charAt(i));
+            int a = Integer.parseInt(min);
+            int b = a+a;
+            System.out.println(b); 
+        }
+
+        sc.close();
+    }
+}
